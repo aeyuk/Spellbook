5b2ec3bacdba8ec56b8212786a07ee9868c128c6
Cate Yuk <aeyuk@crimson.ua.edu>
2018-06-04 21:41:51 -0500

    Execute on enter as well as click

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const button = document.querySelector('button');
+const input = document.querySelector('input');
 
 function changeHeading() {
     const heading2 = document.querySelector('#spell2')
-    heading2.textContent = document.querySelector('#newSpell').value;
+    heading2.textContent = input.value;
 }
 
 button.addEventListener('click', changeHeading);
 
 
+input.addEventListener('keyup', function(event) {
+    if (event.keyCode === 13) {
+        button.click();
+    }
+});
