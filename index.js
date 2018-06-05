67bbac9690cb29053da6a2f6995026529aa88f74
Cate Yuk <aeyuk@crimson.ua.edu>
2018-06-04 20:47:06 -0500

    Replace heading 2 with user-input in form

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const button = document.querySelector('button');
 
 function changeHeading() {
-    const heading2 = document.querySelector('#spell2');
-    heading2.textContent = "Cast";
+    const heading2 = document.querySelector('#spell2')
+    heading2.textContent = document.querySelector('#newSpell').value;
 }
+
 button.addEventListener('click', changeHeading);
 
 
