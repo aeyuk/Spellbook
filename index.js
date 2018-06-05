1b2eda3acad7cc45000bdef075b261a0cb37a2bf
Cate Yuk <aeyuk@crimson.ua.edu>
2018-06-05 17:01:17 -0500

    Add two user-entered texts in a spell list and level list

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
-const button = document.querySelector('button'); //
+/*
+const button = document.querySelector('button');
 const input = document.querySelector('input');
 
-function changeHeading() {
-    const heading2 = document.querySelector('#spell2')
-    heading2.textContent = input.value;
+//Change name of title
+const changeHeading = function(ev) {
+    const f = ev.target;
+    const heading = document.querySelector('#title')
+    heading.textContent = input.value;
+    f.reset();
 }
 
-button.addEventListener('click', changeHeading);
+changeButton.addEventListener('click', changeHeading);
 
 document.querySelector('form').addEventListener('submit', function(e) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-        changeHeading(); 
+    e.preventDefault();
+    if (e.keyCode === 13) {
+        changeHeading;
     }
 });
+*/
 
+//Add spell to list
+const AddToList = function(ev) {
+  ev.preventDefault()
+
+  const f = ev.target
+
+  const spellName = f.spellName.value;
+  const spellLevel = f.spellLevel.value;
+
+  const spellsList = document.querySelector('#s');
+  spellsList.innerHTML += `<li>${spellName}</li>`;
+
+  const levelsList = document.querySelector('#l');
+  levelsList.innerHTML += `<li>${spellLevel}</li>`;
+
+  f.reset()
+}
+
+const form = document.querySelector('form')
+form.addEventListener('submit', AddToList)
