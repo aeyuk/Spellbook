47d65e6babd032718575cd5044169f250e37a86b
Cate Yuk <aeyuk@crimson.ua.edu>
2018-06-04 19:02:21 -0500

    Button click changes header

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('testing');
+const button = document.querySelector('button');
+
+function changeHeading() {
+    const heading = document.querySelector('#spell1');
+    heading.textContent = "Cast";
+}
+button.addEventListener('click', changeHeading);
+
+
