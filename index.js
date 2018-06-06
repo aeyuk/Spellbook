3e2142853907f90bffb8457744b9c5592dbf61bc
Cate Yuk <aeyuk@crimson.ua.edu>
2018-06-06 17:04:38 -0500

    Build each element in separate functions (modeled off class)

--- index.js
+++ index.js
@@ -1,54 +1,33 @@
-/*const makeListElement = function() {
-  const el = document.createElement('span');
-  el.textContent = value;
-  el.setAttribute('title', value);
-  const listElement = document.createElement("LI");
-  return listElement;
-}
-
-
 const makeSpanElement = function(name, value) {
   const el = document.createElement('span');
-  el.classList.add('name'); //add class to span
+  el.classList.add(name); //add class to span
   el.textContent = value; //change text
-  el.setAttribute('title', value); //???
-//call with const variable = renderProperty('level', level);
   return el;
 }
-*/ 
+
+
+const makeListElement = function(nameSpan, timeSpan) {
+  const el = document.createElement('li');
+  el.classList.add('spell');
+  el.appendChild(nameSpan);
+  el.appendChild(timeSpan);
+  return el;
+}
+
 
 const addToList = function(ev) {
   ev.preventDefault();
 
   const f = ev.target;
-  let flag = true;
 
-    const nameSpan = document.createElement('span');
-    nameSpan.classList.add('name');
-    nameSpan.textContent = f.spellName.value;
 
-    const timeSpan = document.createElement('span');
-    timeSpan.classList.add('time');
-    timeSpan.textContent = f.spellTime.value;
+    const nameSpan = makeSpanElement('name', f.spellName.value);
+    const timeSpan = makeSpanElement('time', f.spellTime.value);
 
-    const listItem = document.createElement('li');
-    listItem.classList.add('spell');
-    listItem.appendChild(nameSpan);
-    listItem.appendChild(timeSpan);
+    const listItem = makeListElement(nameSpan, timeSpan);
 
     const spellList = document.querySelector('#s').appendChild(listItem);
 
-/*
-    const SN = makeListElement();
-    const spellName = document.createTextNode(f.spellName.value);
-    SN.appendChild(spellName);
-    const spellsList = document.querySelector('#s').appendChild(SN);
-
-    const ST = makeListElement();
-    const spellTime = document.createTextNode(f.spellTime.value);
-    ST.appendChild(spellTime);
-    const TimesList = document.querySelector('#t').appendChild(ST);
-*/
     f.reset()
   }
 
