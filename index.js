a95ca23eaf85e2d36b90b64d898976f01fcfdf87
Cate Yuk <aeyuk@crimson.ua.edu>
2018-06-06 08:57:46 -0500

    Add separate makeListElement function

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-//Add spell to list
-const addToSpellList = function(ev) {
+function makeListElement() {
+  const listElement = document.createElement("LI");
+  return listElement;
+}
+
+
+const addToList = function(ev) {
   ev.preventDefault();
 
   const f = ev.target;
@@ -12,32 +17,45 @@ const addToSpellList = function(ev) {
   }
 
   if (flag) {
-    const SN = document.createElement("LI");
+    const SN = makeListElement();
     const spellName = document.createTextNode(f.spellName.value);
     SN.appendChild(spellName);
     const spellsList = document.querySelector('#s').appendChild(SN);
 
-    const ST = document.createElement("LI");
+    const ST = makeListElement();
     const spellTime = document.createTextNode(f.spellTime.value);
     ST.appendChild(spellTime);
     const TimesList = document.querySelector('#t').appendChild(ST);
 
+
+    /*
+    const spellName = document.createTextNode(f.spellName.value);
+    const SN = makeListElement().appendChild(spellName);
+    const namesList = document.querySelector('#s').appendChild(SN);
+    
+    const spellTime = document.createTextNode(f.spellTime.value);
+    const ST = makeListElement().appendChild(spellTime);
+    const timesList = document.querySelector('#t').appendChild(ST);
+  */
+  
+    /*
+    const spellsList = document.querySelector('#s')
+            .appendChild(makeListElement()
+            .appendChild(document
+            .createTextNode(f
+            .spellName.value
+            )));
+
+    const timesList = document.querySelector('#t')
+            .appendChild(makeListElement()
+            .appendChild(document
+            .createTextNode(f
+            .spellTime.value
+            )));
+*/
     f.reset()
   }
 }
 
-const addToTimeList = function(ev) {
-    ev.preventDefault();
-
-    const f = ev.target;
-    const ST = document.createElement("LI");
-    const spellTime = document.createTextNode(f.spellTime.value);
-    ST.appendChild(spellTime);
-    const TimesList = document.querySelector('#t').appendChild(ST);
-
-     f.reset();
-}
-
 const form = document.querySelector('form');
-form.addEventListener('submit', addToSpellList);
-form.addEventListener('submit', addToTimeList)
+form.addEventListener('submit', addToList);
