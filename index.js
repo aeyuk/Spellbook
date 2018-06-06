b5f0215f19626a328bfe34c895b6d1b8e7591d56
Cate Yuk <aeyuk@crimson.ua.edu>
2018-06-05 19:56:08 -0500

    Change difficulty list to time list; Add css images

--- index.js
+++ index.js
@@ -26,14 +26,15 @@ const AddToList = function(ev) {
 
   const f = ev.target
 
-  const spellName = f.spellName.value;
-  const spellLevel = f.spellLevel.value;
-
-  const spellsList = document.querySelector('#s');
-  spellsList.innerHTML += `<li>${spellName}</li>`;
-
-  const levelsList = document.querySelector('#l');
-  levelsList.innerHTML += `<li>${spellLevel}</li>`;
+  const SN = document.createElement("LI");
+  const spellName = document.createTextNode(f.spellName.value);
+  SN.appendChild(spellName);
+  const spellsList = document.querySelector('#s').appendChild(SN);
+                        
+  const ST = document.createElement("LI");
+  const spellTime = document.createTextNode(f.spellTime.value);
+  ST.appendChild(spellTime);
+  const TimesList = document.querySelector('#t').appendChild(ST);
 
   f.reset()
 }
