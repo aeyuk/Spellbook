cb403084467b1c226edb21ac80df92ab35321469
Cate Yuk <aeyuk@crimson.ua.edu>
2018-06-05 21:06:41 -0500

    Change font and add character limit

--- index.js
+++ index.js
@@ -1,42 +1,29 @@
-/*
-const button = document.querySelector('button');
-const input = document.querySelector('input');
-
-//Change name of title
-const changeHeading = function(ev) {
-    const f = ev.target;
-    const heading = document.querySelector('#title')
-    heading.textContent = input.value;
-    f.reset();
-}
-
-changeButton.addEventListener('click', changeHeading);
-
-document.querySelector('form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    if (e.keyCode === 13) {
-        changeHeading;
-    }
-});
-*/
-
 //Add spell to list
 const AddToList = function(ev) {
   ev.preventDefault()
 
   const f = ev.target
+  let flag = true;
 
-  const SN = document.createElement("LI");
-  const spellName = document.createTextNode(f.spellName.value);
-  SN.appendChild(spellName);
-  const spellsList = document.querySelector('#s').appendChild(SN);
-                        
-  const ST = document.createElement("LI");
-  const spellTime = document.createTextNode(f.spellTime.value);
-  ST.appendChild(spellTime);
-  const TimesList = document.querySelector('#t').appendChild(ST);
+  if (f.spellName.value.length > 15) {
+      flag = false;
+      alert("Error: exceeds character limit");
+      f.reset();
+  }
 
-  f.reset()
+  if (flag) {
+    const SN = document.createElement("LI");
+    const spellName = document.createTextNode(f.spellName.value);
+    SN.appendChild(spellName);
+    const spellsList = document.querySelector('#s').appendChild(SN);
+
+    const ST = document.createElement("LI");
+    const spellTime = document.createTextNode(f.spellTime.value);
+    ST.appendChild(spellTime);
+    const TimesList = document.querySelector('#t').appendChild(ST);
+
+    f.reset()
+  }
 }
 
 const form = document.querySelector('form')
