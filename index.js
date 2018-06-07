46ea71ba5119b7978ab8c65080c1843cc60dc792
Cate Yuk <aeyuk@crimson.ua.edu>
2018-06-07 13:54:23 -0500

    Restructure code to be ready to have more buttons (in-class following)

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-//const spellArray = [];
 const app = {
   init: function() {
     this.spellArray = [];
+    this.template = document.querySelector('.spell.template');
+
     const form = document.querySelector('form');
     form.addEventListener('submit', ev => {
+      ev.preventDefault();
       this.addToList(ev);
     })
   },
@@ -13,34 +15,30 @@ const app = {
     const el = document.createElement('span');
     el.classList.add(name); //add class to span
     el.textContent = value; //change text
+    el.setAttribute('title', value);
     return el;
   },
 
   makeListElement: function(spell) {
-    spellProperties = Object.keys(spell);
+    const item = this.template.cloneNode(true);
+    item.classList.remove('template');
 
-    const spanArray = spellProperties.map(prop => {
-      return this.makeSpanElement(prop, spell[prop])
-    })
+    const spellProperties = Object.keys(spell);
 
-    const listItem = document.createElement('li');
-    listItem.classList.add('spell');
-
-    spanArray.forEach(el => {
-      listItem.appendChild(el);
+    spellProperties.forEach(property => {
+      const el = item.querySelector(`.${property}`);
+      el.textContent = spell[property]
+      el.setAttribute('title', spell[property])
     });
-    
-    //Create and append button
-    const button = document.createElement('button');
-    button.setAttribute("class", "w3-circle");
-    listItem.appendChild(button);
-    button.textContent = "Delete";
-    button.addEventListener('click', this.removeSpell.bind(this, spell));
 
-    listItem.appendChild(button);
+    item.querySelector('button.delete')
+        .addEventListener('click', this.removeSpell.bind(this, spell));
 
-    return listItem;
+    //////////////
+
+    return item;
   },
+
 
   addToList: function(ev) {
     ev.preventDefault();
@@ -57,31 +55,10 @@ const app = {
 
     
     const listItem = this.makeListElement(spell);
-
-
-    
-    
-    //Push list items into array
-    /*spellArray.push(listItem.textContent);
-    console.log(spellArray);
-
-    //Remove list item from array on click
-    //Doesn't allow for exact duplicates
-      button.addEventListener('click', function(ev) {
-      listItem.parentNode.removeChild(listItem);
-      for (let i = 0; i < spellArray.length; i++) {
-        if (spellArray[i] === listItem.textContent)
-          spellArray.splice(i, 1);
-      }
-      console.log(spellArray);
-    });
-    */
-    
     const spellList = document.querySelector('#s').appendChild(listItem);
 
-
     f.reset();
-
+    f.spellName.focus;
   },
 
   removeSpell: function(spell, ev) {
